ecebe21973ad9ed6f2a052f9ec7dcfb2081e6f03
trey-sumida <54917302+trey-sumida@users.noreply.github.com>
2020-05-12 10:22:05 -1000

    l

--- voteoff-project/voteOff/settings.py
+++ voteoff-project/voteOff/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'voteOff.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'root',
+        'PASSWORD': 'Oreo808.',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
