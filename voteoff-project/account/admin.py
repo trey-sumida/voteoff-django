dc7e1b8cad7a9f034fe3a8c81d47424b9d450164
trey-sumida <54917302+trey-sumida@users.noreply.github.com>
2020-05-13 14:59:19 -1000

    improved admin site

--- voteoff-project/account/admin.py
+++ voteoff-project/account/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import Account
+from django.contrib.auth.admin import UserAdmin
 
-admin.site.register(Account)
+class AccountAdmin(UserAdmin):
+    list_display = ('username', 'email', 'date_joined', 'last_login', 'is_admin')
+    search_fields = ('username', 'email')
+    readonly_fields = ('date_joined', 'last_login')
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(Account, AccountAdmin)
