c44b9d70fb1aa6fce3e1f0b1c37bde7d9bafad85
trey-sumida <54917302+trey-sumida@users.noreply.github.com>
2020-05-30 14:53:08 -1000

    removed unnecessary static

--- voteoff-project/voteOff/settings.py
+++ voteoff-project/voteOff/settings.py
@@ -28,7 +28,6 @@ DEBUG = False
 ALLOWED_HOSTS = ['app.thevoteoff.com', '157.245.183.89', 'localhost']
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "pages", "static"),
     os.path.join(BASE_DIR, "account", "static"),
     os.path.join(BASE_DIR, "lists", "static"),
 ]
