4a647b87a965dd0cb18a5ec363bbe6e4d956bece
trey-sumida <54917302+trey-sumida@users.noreply.github.com>
2020-06-20 15:05:16 -1000

    reverted database back to old one

--- voteoff-project/voteOff/settings.py
+++ voteoff-project/voteOff/settings.py
@@ -81,13 +81,12 @@ WSGI_APPLICATION = 'voteOff.wsgi.application'
 DATABASES = {
 'default': {
     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'NAME': 'doadmin',
+    'NAME': 'voteoffdb',
     'USER': 'tsumida',
-    'PASSWORD': 'o3falh3xhv12dpu8',
-    'HOST': 'db-thevoteoff-do-user-7565072-0.a.db.ondigitalocean.com',
-    'PORT': '25060',
-    'database' = 'defaultdb',
-    'sslmode' = 'require',
+    'PASSWORD': 'Underdog24.',
+    'HOST': 'localhost',
+    'PORT': '',
+    }
 }
 
 
